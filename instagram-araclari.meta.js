1effd1788a2f2698ab7036c7bb80e8d06e8b9f3f
lujiangz <31911561+lujiangz@users.noreply.github.com>
2020-03-11 00:00:14 +0300

    Add files via upload

---
+++ instagram-araclari.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Nega Instagram Araçları
+// @namespace    https://nebigarci.net
+// @author       Nebi GARCI
+// @match        https://www.instagram.com/*
+// @version      2.8.6
+// @description  Instagram hesabınızı daha etkin kullanmanız için Nebi Garcı tarafından hazırlanmıştır.
+// @icon         http://www.nebigarci.net/wp-content/uploads/eklentiler/logonia.png
+// @connect      nebigarci.net
+// @grant        GM_xmlhttpRequest
+// @require      https://code.jquery.com/jquery-1.8.3.min.js
+// @require      https://nebigarci.net/eklenti/instapi.js
+// @require     https://raw.githubusercontent.com/blueimp/JavaScript-MD5/master/js/md5.min.js
+// @updateURL    https://nebigarci.net/eklenti/nia.meta.js
+// @downloadURL    https://nebigarci.net/nia
+// ==/UserScript==
